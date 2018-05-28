4c12e4b2da0fb9674454274aa26702c6fc112aea
ragoragino <kiselrastislav@gmail.com>
2018-05-28 19:08:04 +0200

    Small adjustments

--- server/server/include/ClientHandler.h
+++ server/server/include/ClientHandler.h
@@ -10,11 +10,14 @@ namespace Chat
 			ClientHandler(const ADDR_SOCKET& addr_socket_in) :
 				addr_socket(addr_socket_in) {};
 
+			// Function handling receiving of the messages
 			int32_t receiver();
 
 		private:
+			// Helper function handling the completeness of transfer of a message
 			int32_t recvall(char *message, int32_t messageSize, int32_t flags);
 
+			// Function responsible for closing a socket
 			int32_t closeSocket(int8_t flag);
 
 			ADDR_SOCKET addr_socket;
